4ddf2d8a63b8eeafb7f91e69b869c7aa9f7cb636
Gary Hetzel <garyhetzel@gmail.com>
2013-04-16 12:47:03 -0400

    0.0.2: added LVM to disk metrics

--- lib/reporter/default/stats_base.rb
+++ lib/reporter/default/stats_base.rb
@@ -4,46 +4,6 @@
 
 report do
 # ------------------------------------------------------------------------------
-# filesystem
-#
-  stats_fs = {}
-  current_dev = nil
-
-# logical mounts
-  File.open("/etc/mtab").each do |line|
-    dev,mount,fstype,flags,dump,pass = line.split(/\s+/)
-
-    stats_fs[dev] = {
-      :mount      => mount,
-      :device     => dev,
-      :filesystem => fstype,
-      :flags      => flags.split(/\s*,\s*/)
-    }
-  end
-
-# logical space utilization
-  IO.popen("df").lines.each do |line|
-    next if line =~ /^Filesystem/
-    parts = line.split(/\s+/)
-
-    if parts.length == 1
-      current_dev = parts.first
-      next
-
-    else
-      dev,kblk,used,free,percent,mount = parts
-      dev = current_dev if dev.empty?
-      next unless stats_fs[dev] and stats_fs[dev].is_a?(Hash)
-
-      stats_fs[dev][:used] = (used.to_i * 1024)
-      stats_fs[dev][:available] = (free.to_i * 1024)
-      stats_fs[dev][:total] = (stats_fs[dev][:available] + stats_fs[dev][:used])
-      stats_fs[dev][:percent_used] = percent.delete('%').to_i
-    end
-  end
-
-
-# ------------------------------------------------------------------------------
 # memory
 #
   stats_mem = {}
@@ -85,10 +45,10 @@ report do
 # ------------------------------------------------------------------------------
 # set stat properties
 #
-  stat :disk, stats_fs.select{|k,v| k =~ /^\/dev\/((h|s|xv|v)d|mapper)/ }.values rescue []
   stat :memory, stats_mem
   stat :cpu, ({
-    'count' => stats_cpu[:count].to_i,
-    'speed' => stats_cpu[:processors].collect{|i| i[:speed] }.compact.uniq.sort{|a,b| a.to_f <=> b.to_f }.last.to_f
+    'count'    => Facter.value('processorcount'),
+    'physical' => Facter.value('physicalprocessorcount'),
+    'speed'    => stats_cpu[:processors].collect{|i| i[:speed] }.compact.uniq.sort{|a,b| a.to_f <=> b.to_f }.last.to_f
   } rescue nil)
 end
---
+++ lib/reporter/default/stats_disk.rb
@@ -0,0 +1,110 @@
+# Onering Collector - Disk Statistics plugin
+#   provides data on disks, mounts, and RAID configuration
+#
+
+report do
+# ------------------------------------------------------------------------------
+# mounts
+#
+  mounts = {}
+  current_dev = nil
+
+  File.open("/etc/mtab").each do |line|
+    dev,mount,fstype,flags,dump,pass = line.split(/\s+/)
+
+    mounts[dev] = {
+      :mount      => mount,
+      :device     => dev,
+      :filesystem => fstype,
+      :flags      => flags.split(/\s*,\s*/)
+    }
+  end
+
+# logical space utilization
+  IO.popen("df").lines.each do |line|
+    next if line =~ /^Filesystem/
+    parts = line.split(/\s+/)
+
+    if parts.length == 1
+      current_dev = parts.first
+      next
+
+    else
+      dev,kblk,used,free,percent,mount = parts
+      dev = current_dev if dev.empty?
+      next unless mounts[dev] and mounts[dev].is_a?(Hash)
+
+      mounts[dev][:used] = (used.to_i * 1024)
+      mounts[dev][:available] = (free.to_i * 1024)
+      mounts[dev][:total] = (mounts[dev][:available] + mounts[dev][:used])
+      mounts[dev][:percent_used] = percent.delete('%').to_i
+    end
+  end
+
+
+# ------------------------------------------------------------------------------
+# LVM
+#
+  vg = {}
+
+# volume groups
+  IO.popen("vgdisplay -c").lines.each do |line|
+    line = line.strip.chomp.split(':')
+
+    vg[line[0]] = {
+      :name    => line[0],
+      :uuid    => line[16],
+      :size    => (line[11].to_i*1024),
+      :extents => {
+        :size      => (line[12].to_i * 1024),
+        :total     => line[13].to_i,
+        :allocated => line[14].to_i,
+        :free      => line[15].to_i
+      },
+      :volumes => [],
+      :disks => []
+    }
+  end
+
+# logical volumes
+  IO.popen("lvdisplay -c").lines.each do |line|
+    line = line.strip.chomp.split(':')
+
+    unless vg[line[1]].nil?
+      vg[line[1]][:volumes] << {
+        :name    => line[0],
+        :sectors => line[6].to_i,
+        :extents => line[7].to_i,
+        :size    => (vg[line[1]][:extents][:size] * line[7].to_i)
+      }
+    end
+  end
+
+
+# physical volumes
+  IO.popen("pvdisplay -c").lines.each do |line|
+    line = line.strip.chomp.split(':')
+
+    unless vg[line[1]].nil?
+      vg[line[1]][:disks] << {
+        :name    => line[0],
+        :uuid    => line[11].to_i,
+        :size    => (line[2].to_i * 1024),
+        :extents => {
+          :size      => (line[7].to_i * 1024),
+          :total     => line[8].to_i,
+          :allocated => line[10].to_i,
+          :free      => line[9].to_i
+        }
+      }
+    end
+  end
+
+
+  stat :disk, {
+    :mounts => (Hash[mounts.select{|k,v| k =~ /^\/dev\/((h|s|xv|v)d|mapper)/ }].values rescue nil),
+    :lvm    => {
+      :groups => vg.values
+    }
+  }
+end
