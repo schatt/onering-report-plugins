fd0fe98941e6fb5754ea259eb4db15b624090c8a
Gary Hetzel <garyhetzel@gmail.com>
2013-04-24 09:53:37 -0400

    Removed netstat reporting

--- lib/reporter/default/stats_network.rb
+++
@@ -1,9 +0,0 @@
-# Onering Collector - Network Statistics plugin
-#   provides network details to the Onering API
-#
-
-report do
-  stat :network, {
-    :sockets => Facter.value('netstat')
-  }
-end
